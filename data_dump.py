93ba3176680fe612780b8d93cafe323f9e5c2f5e
07Sada <sadashiv.nandanikar07@gmail.com>
2023-02-13 13:23:37 +0530

    data_dump.py created

---
+++ data_dump.py
@@ -0,0 +1,32 @@
+import pymongo
+import pandas as pd 
+import json 
+
+# Import the mongo_client object from the restr_rating.config module
+from restr_rating.config import mongo_client
+
+# Define the path to the data file
+DATA_FILE_PATH = "/config/workspace/zomato.csv"
+
+# Define the name of the database and collection to use
+DATABASE_NAME = 'restr_ratings'
+COLLECTION_NAME = 'ratings'
+
+if __name__=="__main__":
+    # Read the data file using pandas
+    df = pd.read_csv(DATA_FILE_PATH)
+    
+    # Print the shape of the data to verify that it has been loaded correctly
+    print(f"Shape of the data: {df.shape} ")
+
+    # Reset the index of the dataframe
+    df.reset_index(drop=True, inplace=True)
+
+    # Convert the dataframe into a list of dictionaries in JSON format
+    json_record = list(json.loads(df.T.to_json()).values())
+
+    # Insert the list of dictionaries into MongoDB
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    
+    # Print a message indicating that the data has been successfully dumped to the database
+    print("Records Successfully Dumbed to DB")
---
+++ restr_rating/config.py
@@ -0,0 +1,18 @@
+import pymongo
+import pandas as pd 
+import numpy as np 
+import json 
+from dataclasses import dataclass 
+import os 
+from dotenv import load_dotenv
+
+# load environment variable from a .env file
+load_dotenv()
+
+@dataclass
+class EnvironmentVariable:
+    mongodb_url:str = os.getenv("MONGO_DB_URL")
+
+env_var = EnvironmentVariable()
+
+mongo_client = pymongo.MongoClient(env_var.mongodb_url)
