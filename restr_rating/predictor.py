ad9474ad01f817cea19e3fed579ee3fc913be8b4
07Sada <sadashiv.nandanikar07@gmail.com>
2023-02-21 15:58:30 +0530

    updated

--- restr_rating/predictor.py
+++ restr_rating/predictor.py
@@ -14,26 +14,104 @@ class ModelResolver:
         os.makedirs(self.model_registry, exist_ok=True)
         self.model_dir_name = model_dir_name
 
-    def get_latest_dir_path(self) -> Optional[str]:
+    def get_latest_dir_path(self)->Optional[str]:
         try:
             dir_names = os.listdir(self.model_registry)
-            if len(dir_names) == 0:
+            if len(dir_names)==0:
                 return None
-            dir_names = list(map(int, dir_names))
+            dir_names = list(map(int,dir_names))
             latest_dir_name = max(dir_names)
-            return os.path.join(self.model_registry, f"{latest_dir_name}")
+            return os.path.join(self.model_registry,f"{latest_dir_name}")
         except Exception as e:
-                raise RatingException(e, sys)
-    
+            raise e
+
     def get_latest_model_path(self):
         try:
             latest_dir = self.get_latest_dir_path()
             if latest_dir is None:
                 raise Exception(f"Model is not available")
-            return os.path.join(latest_dir, self.model_dir_name, MODEL_FILE_NAME)
+            return os.path.join(latest_dir,self.model_dir_name,MODEL_FILE_NAME)
         except Exception as e:
-            raise e 
+            raise e
+    
+    def get_latest_save_dir_path(self)->str:
+        try:
+            latest_dir = self.get_latest_dir_path()
+            if latest_dir==None:
+                return os.path.join(self.model_registry,f"{0}")
+            latest_dir_num = int(os.path.basename(self.get_latest_dir_path()))
+            return os.path.join(self.model_registry,f"{latest_dir_num+1}")
+        except Exception as e:
+            raise e
 
+    def get_latest_save_model_path(self):
+        try:
+            latest_dir = self.get_latest_save_dir_path()
+            return os.path.join(latest_dir,self.model_dir_name,MODEL_FILE_NAME)
+        except Exception as e:
+            raise e
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################################################################################################
+    # def get_latest_dir_path(self) -> Optional[str]:
+    #     try:
+    #         dir_names = os.listdir(self.model_registry)
+    #         if len(dir_names) == 0:
+    #             return None
+    #         dir_names = list(map(int, dir_names))
+    #         latest_dir_name = max(dir_names)
+    #         return os.path.join(self.model_registry, f"{latest_dir_name}")
+    #     except Exception as e:
+    #             raise RatingException(e, sys)
+    
+    # def get_latest_model_path(self):
+    #     try:
+    #         latest_dir = self.get_latest_dir_path()
+    #         if latest_dir is None:
+    #             raise Exception(f"Model is not available")
+    #         return os.path.join("1", self.model_dir_name, MODEL_FILE_NAME)
+    #     except Exception as e:
+    #         raise e 
+###############################################################################################
     # def get_latest_transformer_path(self):
     #     try:
     #         latest_dir = self.get_latest_dir_path()
@@ -42,24 +120,24 @@ class ModelResolver:
     #         return os.path.join(latest_dir, self.transformer_dir_name, TRANSFORMER_OBJECT_FILE_NAME)
     #     except Exception as e:
     #         raise e 
-    
-    def get_latest_save_dir_path(self)->str:
-        try:
-            latest_dir = self.get_latest_dir_path()
-            if latest_dir ==None:
-                return os.path.join(self.model_registry,f"{0}")
-            latest_dir_num = int(os.path.basename(self.get_latest_dir_path()))
-            return os.path.join(self.model_registry,f"{latest_dir_num+1}")
-        except Exception as e:
-            raise e
+    ###########################################################################################
+    # def get_latest_save_dir_path(self)->str:
+    #     try:
+    #         latest_dir = self.get_latest_dir_path()
+    #         if latest_dir ==None:
+    #             return os.path.join(self.model_registry,f"{0}")
+    #         latest_dir_num = int(os.path.basename(self.get_latest_dir_path()))
+    #         return os.path.join(self.model_registry,f"{latest_dir_num+1}")
+    #     except Exception as e:
+    #         raise e
             
-    def get_latest_save_model_path(self):
-        try:
-            latest_dir = self.get_latest_save_dir_path()
-            return os.path.join(latest_dir,self.model_dir_name,MODEL_FILE_NAME)
-        except Exception as e:
-            raise e
-
+    # def get_latest_save_model_path(self):
+    #     try:
+    #         latest_dir = self.get_latest_save_dir_path()
+    #         return os.path.join(latest_dir,self.model_dir_name,MODEL_FILE_NAME)
+    #     except Exception as e:
+    #         raise e
+######################################################################################################
     # def get_latest_save_transformer_path(self):
     #     try:
     #         latest_dir = self.get_latest_save_dir_path()
